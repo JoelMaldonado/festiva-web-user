d3a0c07127d404383568f64074bb4ff7fdf0580a
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-01-09 09:57:10 -0500

    fix: actualizar la URL de la API a la raíz

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://festiva.no/user/api',
+  apiUrl: 'https://festiva.no/user',
   mapboxToken:
     'pk.eyJ1IjoiamptZmRldiIsImEiOiJjbTgybXg0YjUxbGs1MmpxM210OTdmbTE2In0.AjmCpDHaiKjNj-XosGXXuA',
 };
