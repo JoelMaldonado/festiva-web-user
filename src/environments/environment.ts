66cf4f2b7d5cc828bb707a0ad0e953c3c1833cab
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-01-09 09:51:00 -0500

    fix: actualizar la URL de la API en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:1234',
+  apiUrl: 'http://localhost:8002',
   mapboxToken:
     'pk.eyJ1IjoiamptZmRldiIsImEiOiJjbTgybXg0YjUxbGs1MmpxM210OTdmbTE2In0.AjmCpDHaiKjNj-XosGXXuA',
 };
