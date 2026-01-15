6f594639df0a94fdda672cc233f5343a4272b22a
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-01-15 18:04:04 -0500

    fix: corregir URL de la API en el entorno de configuración

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://festiva.no/user',
+  apiUrl: 'https://api.festiva.no/user',
   mapboxToken:
     'pk.eyJ1IjoiamptZmRldiIsImEiOiJjbTgybXg0YjUxbGs1MmpxM210OTdmbTE2In0.AjmCpDHaiKjNj-XosGXXuA',
 };
