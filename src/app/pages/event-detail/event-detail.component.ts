95764b2158af679745753c3c6bd123ce39965340
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-01-30 10:15:17 -0500

    feat: agregar manejo de título y meta descripción en el componente de detalle del evento fix: corregir URL de la API en el entorno de desarrollo

--- src/app/pages/event-detail/event-detail.component.ts
+++ src/app/pages/event-detail/event-detail.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { EventAboutComponent } from './components/about.component';
 import { EventScheduleComponent } from './components/schedule.component';
 import { EventMapComponent } from './components/map.component';
@@ -12,6 +12,7 @@ import { EventService } from '../../services/event.service';
 import { Event } from '../../core/models/event.model';
 import { ClubService } from '../../services/club.service';
 import { Club } from '../../core/models/club.model';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-event-detail',
@@ -31,6 +32,10 @@ import { Club } from '../../core/models/club.model';
 export class EventDetailComponent implements OnInit {
   private readonly eventService = inject(EventService);
   private readonly clubService = inject(ClubService);
+  private readonly titleSrv = inject(Title);
+  private readonly metaSrv = inject(Meta);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
 
   @Input({ required: true }) eventId!: string;
 
@@ -47,6 +52,26 @@ export class EventDetailComponent implements OnInit {
       next: (res) => {
         if (res.success) {
           this.event = res.data;
+
+          // Title
+          this.titleSrv.setTitle(`${this.event.title} | Festiva`);
+
+          // Meta Description
+          const desc = (this.event.description || '').replace(/\s+/g, ' ').trim().slice(0, 160);
+
+          if (desc) {
+            this.metaSrv.updateTag({ name: 'description', content: desc });
+          }
+
+          const currentSlug = this.route.snapshot.paramMap.get('slug');
+          const expectedSlug = slugify(this.event.title);
+
+          if (currentSlug !== expectedSlug) {
+            this.router.navigate(['/events', this.event.id, expectedSlug], {
+              replaceUrl: true,
+            });
+          }
+
           this.getClub(this.event!.club_id);
         } else {
           console.error('Failed to load event', res.message);
@@ -90,4 +115,14 @@ export class EventDetailComponent implements OnInit {
   onNextDateChange(date?: Date) {
     this.nextDate = date;
   }
+}
+
+function slugify(input: string) {
+  return (input || '')
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '') // quita acentos
+    .replace(/[^a-z0-9]+/g, '-') // símbolos/espacios -> "-"
+    .replace(/(^-|-$)+/g, '') // trim "-"
+    .slice(0, 90);
 }
