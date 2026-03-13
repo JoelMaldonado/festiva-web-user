3d682d49c109a29daf244691ff2fba444c9bba48
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-03-13 13:06:05 -0500

    feat: update budget limits in angular.json and enhance server routes in app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'events/:eventId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'events/:eventId/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'clubs/:clubId',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
